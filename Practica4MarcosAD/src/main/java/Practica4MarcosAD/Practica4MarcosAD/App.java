2428d99283112eac0b57e07ad97f032fbb298cf1
ManuelRevuelto <manu.revuelto1111@gmail.com>
2021-03-01 12:14:09 +0100

    Primer commit

---
+++ Practica4MarcosAD/src/main/java/Practica4MarcosAD/Practica4MarcosAD/App.java
@@ -0,0 +1,13 @@
+package Practica4MarcosAD.Practica4MarcosAD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Practica4MarcosAD/src/test/java/Practica4MarcosAD/Practica4MarcosAD/AppTest.java
@@ -0,0 +1,20 @@
+package Practica4MarcosAD.Practica4MarcosAD;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
